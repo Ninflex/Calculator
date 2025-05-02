345eaa5259c39096a726e63c4ec115d19b902efb
Kajus Gajauskas <145608156+Ninflex@users.noreply.github.com>
2025-05-02 14:15:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const display = document.getElementById("display");
+let ans = "";
 
 function appendDisplay(input){
     display.value += input;
-    
     //scrolls with numbers
     display.scrollLeft = display.scrollWidth;
 }
@@ -11,17 +11,56 @@ function clearDisplay(){
     display.value = "";
 }
 
+function previousAnswer(){
+    if(ans === ""){
+        display.value += "0"
+    }
+    else{
+        display.value += ans;
+    }
+}
+
+function advanced() {
+    const panel = document.getElementById("advancedPanel");
+    panel.classList.toggle("visible");
+}
+
 function calculate(){
     try {
-        display.value = Function('return (' + display.value + ')')();
+        ans = math.evaluate(display.value);
+        display.value = ans;
     } catch {
-        if(display.value === "" || display.value === "Error"){
+        if(display.value === ""){
             display.value = ""
-            console.log(display.value)
         }
         else{
             display.value = "Error";
-            console.log(display.value)
         }
     }
 }
+
+//Keyboard inputs
+document.addEventListener('keydown', (event) =>{
+    const key = event.key;
+
+    if(!isNaN(key)){
+        //numbers between 0-9
+        appendDisplay(key);
+    }
+    else if(['+','-','*','/','.'].includes(key)){
+        appendDisplay(key);
+    }
+    else if(key === 'Enter'){
+        event.preventDefault();
+        calculate();
+    }
+    else if(key === 'Backspace'){
+        display.value = display.value.slice(0,-1);
+    }
+    else if(key === 'c'){
+        clearDisplay();
+    }
+    else if(key === 'a'){
+        previousAnswer();
+    }
+})
